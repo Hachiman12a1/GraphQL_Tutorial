1690cabefa717abf79fd311ec4c5f19ea838c4fd
Đào Văn Đạt <datdv@its-global.vn>
2022-11-06 16:27:49 +0700

    Update Resolver, Schema , Add Data to MongoDB

--- data/static.js
+++ data/static.js
@@ -1,40 +1,40 @@
 const books = [
-	{
-		id: 1,
-		name: 'Tat den',
-		genre: 'Tieu thuyet',
-		authorId: 1
-	},
-	{
-		id: 2,
-		name: 'Chi Pheo',
-		genre: 'Truyen ngan',
-		authorId: 2
-	},
-	{
-		id: 3,
-		name: 'So do',
-		genre: 'Tieu thuyet',
-		authorId: 3
-	},
-	{
-		id: 4,
-		name: 'Doi thua',
-		genre: 'Truyen ngan',
-		authorId: 2
-	},
-	{
-		id: 5,
-		name: 'Song mon',
-		genre: 'Tieu thuyet',
-		authorId: 2
-	},
-	{
-		id: 6,
-		name: 'Ky nghe lay Tay',
-		genre: 'Phong su',
-		authorId: 3
-	}
+  {
+    id: 1,
+    name: "Tat den",
+    genre: "Tieu thuyet",
+    authorId: 1,
+  },
+  {
+    id: 2,
+    name: "Chi Pheo",
+    genre: "Truyen ngan",
+    authorId: 2,
+  },
+  {
+    id: 3,
+    name: "So do",
+    genre: "Tieu thuyet",
+    authorId: 3,
+  },
+  {
+    id: 4,
+    name: "Doi thua",
+    genre: "Truyen ngan",
+    authorId: 2,
+  },
+  {
+    id: 5,
+    name: "Song mon",
+    genre: "Tieu thuyet",
+    authorId: 2,
+  },
+  {
+    id: 6,
+    name: "Ky nghe lay Tay",
+    genre: "Phong su",
+    authorId: 3,
+  },
 ];
 
 const authors = [
@@ -56,6 +56,6 @@ const authors = [
 ];
 
 module.exports = {
-    books,
-    authors
-}
+  books,
+  authors,
+};
