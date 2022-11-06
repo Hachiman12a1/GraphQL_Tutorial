c55d3bdc57001eb475046bf09df6bf654091dc39
Đào Văn Đạt <datdv@its-global.vn>
2022-11-06 15:40:27 +0700

    Connect MongoDB

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const express = require("express");
 
 const { ApolloServer } = require("apollo-server-express");
 
+const mongoose = require("mongoose");
+
 // Load schema & resolvers
 const typeDefs = require("./schema/schema");
 const resolvers = require("./resolver/resolver");
+
+// Connect to mongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://datvip10a1:dat682000@grapqltutorial.z5nepir.mongodb.net/?retryWrites=true&w=majority", {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 const server = new ApolloServer({
   typeDefs,
@@ -16,6 +37,6 @@ server.applyMiddleware({
   app,
 });
 
-app.listen({port: 4000}, () => {
-    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
-})
+app.listen({ port: 4000 }, () => {
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+});
